81f60b954286e9609067dee5a8491f824f3af5f7
unknown <arthurferreira1417@gmail.com>
2024-06-20 17:21:04 -0300

    Feat: criando rotas de consulta de um ou todos os usurarios

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,8 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\UserController;
 
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::prefix('Auth')->group( function(){
+
+    Route::get('/user', [UserController::class, 'index'] );
+    Route::get('/user/{user}', [UserController::class, 'show'] );
+}
+);
