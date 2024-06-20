24adc380ecfc0745ff60eaf6e780f7644f23d3f4
unknown <arthurferreira1417@gmail.com>
2024-06-20 11:27:59 -0300

    Feat: configurando as tablas do banco da tabela invoices

---
+++ api/database/migrations/2024_06_20_132604_create_invoices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Invoices', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('type');
+            $table->boolean('paid');
+            $table->decimal('value', 10, 2);
+            $table->dateTime('payment_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Invoices');
+    }
+};
