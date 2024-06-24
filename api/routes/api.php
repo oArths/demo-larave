80c056ccf1eed10d5958afd1238938185ce39d11
unknown <arthurferreira1417@gmail.com>
2024-06-24 15:56:51 -0300

    Feat: criando rotas pra crud e pra criação de token

--- api/routes/api.php
+++ api/routes/api.php
@@ -3,14 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\UserController;
+use App\Http\Controllers\AuthJwtController;
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
 
-Route::prefix('Auth')->group( function(){
-
-    Route::get('/user', [UserController::class, 'index'] );
-    Route::get('/user/{user}', [UserController::class, 'show'] );
-}
-);
+Route::prefix('Auth')->group(function(){
+    // Route::get('/users', [UserController::class, 'index']);
+    // Route::get('/users/{user}', [UserController::class, 'show']);
+    // Route::put('/users/{user}', [UserController::class, 'update']);
+    // Route::post('/users/create', [UserController::class, 'store']);
+    // Route::delete('/users/delete/{user?}', [UserController::class, 'destroy']);
+    Route::resource('users', UserController::class);
+    Route::post('user/create-token', [AuthJwtController::class, 'Create_Jwt']);
+});
