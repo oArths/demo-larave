3b0912a878278263e3e1cc4d4c4adbfbcbf22e07
unknown <arthurferreira1417@gmail.com>
2024-06-24 15:58:08 -0300

    Feat: criando a autenticação do jwt

--- api/app/Http/Middleware/Authenticate.php
+++ api/app/Http/Middleware/Authenticate.php
@@ -3,19 +3,61 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Closure;
 
 class Authenticate extends Middleware
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
-     */
-    protected function redirectTo($request)
+    private $key;
+
+    public function __construct(){
+        $this->key = 'lua';
+    }
+ 
+    
+    public function handle($request, Closure $next, ...$guards)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        $token = $this->getBearerToken($request);
+        if(!$token){
+            return response()->json(['error'=> 'token expiorado ou invalido'], 401);
         }
+        $valid = $this->valid_token($token);
+
+        if(!$valid){
+            return response()->json(['error'=> 'token expiorado ou invalido'], 401);
+        }
+
+        $request->merge(['auth' => (array) $valid]);
+
+        // return $request;
+        return $next($request);
+    }
+  
+    
+    private function getBearerToken($request)
+    {
+        $data = $request->header('Authorization');
+        if(empty($data)){
+            return false;
+        }
+        $token = explode(' ', $data);
+        return  $token[1];
+        
+       
+    }
+    public function valid_token($token){
+
+        list($header, $payload, $sing) = explode('.', $token);
+
+        $dec_payload = json_decode(base64_decode($payload));
+        $valid_sing = base64_encode(hash_hmac('sha256', $header . "." . $payload, $this->key, true));
+
+        if($sing !== $valid_sing){
+            return false;
+        }
+        if($dec_payload->exp < time()){
+            return false;
+        }
+        return $dec_payload;
+
     }
 }
